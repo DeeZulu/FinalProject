63ee49255d8e2672c56fe449ea3763b3e9e1f8b2
Залуцкий Денис <zalutskiydv@pochtabank.ru>
2025-02-07 14:02:51 +1000

    класс MainPageLocators

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
         """Проверка ссылки на логин"""
-        assert self.is_element_present(By.XPATH, "//a[@id='login_link']"), "Ссылка на логин отсутствует"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Ссылка на логин отсутствует"
